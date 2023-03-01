46d1603c09f86ea173bbf49c660e19fd9a229b8d
bk <kkbk0077@gmail.com>
2023-03-01 11:52:16 +0900

    프로젝트 초기 설정

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      spacing: {
+        18: '4.5rem',
+        30: '7.5rem',
+        88: '22rem',
+        104: '26rem',
+        112: '28rem',
+        128: '32rem',
+      },
+      colors: {
+      },
+    },
+  },
+  plugins: [],
+};
