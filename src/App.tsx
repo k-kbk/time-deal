df4e11b4f76944e2fe841719123e49e79dd3ed47
bk <kkbk0077@gmail.com>
2023-03-01 14:16:12 +0900

    feat: react-router-dom, recoil, react-query 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/layout';
+import Home from './components/home';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,26 @@
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './global.css'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { BrowserRouter } from 'react-router-dom';
+import './global.css';
+
+const client = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <App />
-)
+  <RecoilRoot>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  </RecoilRoot>,
+);
