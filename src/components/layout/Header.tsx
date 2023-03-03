34bb73f94773cb47870dfaa06132563955cb2026
bk <kkbk0077@gmail.com>
2023-03-03 12:51:10 +0900

    refactor: 로고에 홈 이동하기 추가

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,14 @@
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className="fixed top-0 flex h-[4rem] w-full items-center justify-between bg-header px-8 sm:px-10">
-      <p className="text-2xl font-bold tracking-tighter text-red-500">
+    <header className="fixed top-0 flex h-[4rem] w-full items-center justify-between bg-header px-2 sm:px-10">
+      <Link
+        to="/"
+        className="py-2 px-4 text-2xl font-bold tracking-tighter text-red-500"
+      >
         NUMBLE.
-      </p>
+      </Link>
       <NavLink
         to="/login"
         className={({ isActive }) =>
